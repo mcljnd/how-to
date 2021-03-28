74b6331e724bedfd3b486ad6edd2a79085d5b683
Michal Janda <michal@janda.name>
2021-03-28 18:22:05 +0200

    update

--- speedtest-cli.cmd
+++ speedtest-cli.cmd
@@ -4,3 +4,4 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
 echo "deb https://ookla.bintray.com/debian generic main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
 apt update
 apt install speedtest
+
